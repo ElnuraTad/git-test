833c57c77ed74ff718da1bcdcfd5ac147ff2b359
My Name <tadzhibaevaelnura@gmail.com>
2021-06-13 02:35:20 +0600

    sout changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a test branch");
+        System.out.println("Software develop");
 
 
     }
